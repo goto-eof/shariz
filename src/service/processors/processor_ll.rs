d8fa6864f84bfff3ffd6effc0db11c19c7ee9c0a
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-09 12:54:08 +0200

    refactor

--- src/service/processors/processor_ll.rs
+++ src/service/processors/processor_ll.rs
@@ -25,13 +25,7 @@ impl CommandProcessor for LLProcessor {
     fn process(&self, full_command: &str, stream: &mut TcpStream) -> bool {
         println!("processing command: {}", full_command);
         let mut files_string = "".to_owned();
-        // let mut lenght = 0;
 
-        // let files_result = fs::read_dir(&self.search_directory);
-        // if files_result.is_err() {
-        //     return false;
-        // }
-        // let files = files_result.unwrap();
         let all_db_files = list_all_files(&self.db_connection_mutex.lock().unwrap()).unwrap();
 
         all_db_files.iter().for_each(|file| {
@@ -43,26 +37,7 @@ impl CommandProcessor for LLProcessor {
                 file.last_update.to_rfc3339()
             );
         });
-        // for file_result in files {
-        //     if file_result.is_err() {_
-        //         return false;
-        //     }
-        //     let file = file_result.unwrap();
 
-        //     if !file.path().ends_with(".DS_Store") && !file.path().is_dir() {
-        //         let file_name = extract_fname(&file.path().to_string_lossy().to_string());
-        //         let status_vec: Vec<&DbFile> = all_db_files
-        //             .iter()
-        //             .filter(|file| file.name.eq(&file_name))
-        //             .collect();
-        //         let mut status = 0;
-        //         if status_vec.len() > 0 {
-        //             status = status_vec.get(0).unwrap().status;
-        //         }
-        //         files_string = format!("{}{};{},", files_string, file_name, status);
-        //         lenght = lenght + 1;
-        //     }
-        // }
         println!("{}", files_string);
         let files = format!("{}\r\n", files_string);
         let write_result = stream.write_all(files.as_bytes());
